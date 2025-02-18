66971c9b18606b3014f821242399e7d882e9f5da
nguyen.van.nhuan <van017nhuan@prestigein.com>
2025-02-18 17:41:26 +0700

    jdbc

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "jdbc:sqlserver://ADMIN-PC\\\\SQLEXPRESS:1433;databaseName=AP;encrypt=false";
+        String user = "sa";
+        String password = "sa";
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             PreparedStatement ps = conn.prepareStatement(
+                     "SELECT * FROM COURSES");
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next())
+                System.out.println(rs.getString(1));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
