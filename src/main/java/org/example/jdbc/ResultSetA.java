56786e97e92761e50e5c37b1f3df3cdb2b268e97
nguyen.van.nhuan <van017nhuan@prestigein.com>
2025-02-25 17:06:00 +0700

    ava

--- src/main/java/org/example/jdbc/ResultSetA.java
+++ src/main/java/org/example/jdbc/ResultSetA.java
@@ -11,16 +11,26 @@ public class ResultSetA {
     public static void main(String[] args) throws SQLException {
         Connection conn = DBContext.getConnection();
         String sql = "SELECT CampusId, CampusCode FROM CAMPUSES";
-        var idToNameMap = new HashMap<Integer, String>();
-        try (var ps = conn.prepareStatement(sql);
+        var sql1 = "SELECT count(*)  FROM CAMPUSES";
+//        var idToNameMap = new HashMap<Integer, String>();
+//        try (var ps = conn.prepareStatement(sql);
+//             ResultSet rs = ps.executeQuery()) {
+//            while (rs.next()) {
+//                int id = rs.getInt("CampusId");
+//                String name = rs.getString("CampusCode");
+//                idToNameMap.put(id, name);
+//
+//            }
+//        }
+//        System.out.println(idToNameMap);
+
+        try (var ps = conn.prepareStatement(sql1);
              ResultSet rs = ps.executeQuery()) {
-            while (rs.next()) {
-                int id = rs.getInt("CampusId");
-                String name = rs.getString("CampusCode");
-                idToNameMap.put(id, name);
+                while (rs.next()) {
+                    int count = rs.getInt(1);
+                    System.out.println(count);
+                }
 
             }
         }
-        System.out.println(idToNameMap);
-    }
 }
